ddf76a62f382a30ee6e7285030ee1f1568deb583
Jay <chriese0112@gmail.com>
2022-10-25 17:51:44 +0800

    update

--- pages/ssrtest.jsx
+++ pages/ssrtest.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from 'react';
+
 export async function getServerSideProps() {
-	const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts/7');
 	const data = await res.json();
 
 	return { props: { post: data } };
 }
 
 export default function Home({ post }) {
+	const [newPost, setNewPost] = useState();
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/posts/1')
+			.then(res => res.json())
+			.then(res => setNewPost(res));
+	}, []);
+	console.log(newPost);
 	return (
 		<div>
 			<h1>{post?.title}</h1>
+			<h2>{newPost?.body}</h2>
 			<p>{post?.body}</p>
 		</div>
 	);
